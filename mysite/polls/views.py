0001ffbe5adf22286e5bcc2e173deac7cd82226a
bodhibrdr <julie.gumerman@gmail.com>
2020-04-16 16:44:43 -0600

    refactored urls and views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Choice, Question
 
 
 # Create your views here.
 from django.http import HttpResponse
-
+#####################ONE WAY TO WRITE INDEX#######################
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -14,7 +15,16 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
-##detail function, first way
+#######################REFACTORED INDEX FUNCTION####################
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+################ detail function, first way ###########################
 # def detail(request, question_id):
 #     try:
 #         question = Question.objects.get(pk=question_id)
@@ -24,14 +34,29 @@ def index(request):
 
 #     #return HttpResponse("You're looking at question %s." % question_id)
 
-##detail function, second way
+
+################### detail function, second way ########################
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
+################### REFACTORED DETAIL VIEW ############################
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+####################results, first way ###############################
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
+
+#################### RESULTS, REFACTORED ###########################
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -47,4 +72,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    #return HttpResponse("You're voting on question %s." % question_id)
+        #HttpResponseRedirect is what bumps you along to the results page when you submit your result
